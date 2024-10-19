77d3b9d6e74b8b383170621367b57079a183edc7
Rory O Loughlin <g00422319@atu.ie>
2024-10-19 17:11:59 +0100

    Committing new project

---
+++ src/main/java/ie/atu/week4cicd/Week4cicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week4cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4cicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week4cicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week4cicd/Week4cicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week4cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4cicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
